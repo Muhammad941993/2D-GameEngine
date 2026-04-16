9098490f79f4912fbe7102c9b29ed86860429947
Muhammad941993 <37065652+Muhammad941993@users.noreply.github.com>
2026-04-16 07:57:44 +0200

    Add Game class and integrate main loop
    Introduce Game.h and Game.cpp to encapsulate SDL initialization, event handling, rendering, and cleanup. Replace the previous ad-hoc SDL setup in main.cpp with a Game instance and a proper run loop;

---
+++ 2d-GameEngine/2D-GameEngine/Game.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <SDL.h>
+#include <iostream>
+
+class Game
+{
+public:
+
+	Game();
+	~Game();
+
+	void Initialize(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+	void HandleEvents();
+	void Update();
+	void Render();	
+	void Clean();
+	bool Running() { return isRunning; }
+
+private:
+
+	bool isRunning;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+
+};
+
--- 2d-GameEngine/2D-GameEngine/main.cpp
+++ 2d-GameEngine/2D-GameEngine/main.cpp
@@ -1,22 +1,25 @@
-#include "SDL.h"
+#include <SDL.h>
+#include "Game.h"
+
+Game *game = nullptr;
 
 
-int main(int argc, char* argv[])
+int main(int argc,char* argv[])
 {
+	
+	game = new Game();
 
-	SDL_Init(SDL_INIT_EVERYTHING);
+	game->Initialize("2D Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
-	SDL_Window* window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+	while (game->Running())
+	{
+		game->HandleEvents();
+		game->Update();
+		game->Render();	
+	}
 
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-
-	SDL_RenderClear(renderer);
-
-	SDL_RenderPresent(renderer);
-
-	SDL_Delay(3000);
-
+	game->Clean();
+	
 	return 0;
 }
+
